9bf63ada181f54dfc6d32b971e67c356b1282117
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-05 23:03:11 +0530

    feat: calculations done and adding footer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 let cgpaIn = document.querySelector("#cgpa");
 let button = document.querySelector("#submit");
 let op = document.querySelector("#output");
+
+function calculate() {
+  let pc = (cgpaIn.value - 0.75) * 10;
+  op.innerHTML = pc;
+}
+
+button.addEventListener("click", calculate);
