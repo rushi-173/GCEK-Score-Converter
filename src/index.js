c7d4caf006a1d2b6f9a2ee40a03c0330dfd53891
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-06 16:53:36 +0530

    update to v 2.0

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ let op = document.querySelector("#output");
 
 function calculate() {
   let pc = (cgpaIn.value - 0.75) * 10;
-  op.innerHTML = pc;
+  if (pc < 0 || pc > 100) {
+    op.innerHTML = "Invalid Input";
+  } else {
+    op.innerHTML = pc;
+  }
 }
 
 button.addEventListener("click", calculate);
